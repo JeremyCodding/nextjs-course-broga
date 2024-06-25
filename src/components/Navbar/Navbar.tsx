9e6f8bac75129fa6bf737dd529f0c337d507fb0f
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-06-25 10:55:35 -0300

    Adding icons to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,14 @@
+import {
+  HomeIcon,
+  GamepadIcon,
+  AwardIcon,
+  WalkPathIcon,
+  UserIcon,
+} from "@/components";
+
 export const Navbar = () => {
   return (
-    <nav className="flex flex-col gap-4 h-screen bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2">
+    <nav className="flex flex-col gap-4 h-screen text-slate-300 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2">
       <div>
         <img
           src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png"
@@ -9,21 +17,26 @@ export const Navbar = () => {
         />
       </div>
       <ul className="my-4 flex-grow border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+        <li className="my-2 flex gap-2 items-center rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <HomeIcon className="w-4 h-4" />
           Home
         </li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+        <li className="my-2 flex gap-2 items-center rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <GamepadIcon className="w-4 h-4" />
           Games
         </li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+        <li className="my-2 flex gap-2 items-center rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <AwardIcon className="w-4 h-4" />
           Top 10
         </li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+        <li className="my-2 flex gap-2 items-center rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <WalkPathIcon className="w-4 h-4" />
           Walkthroughs
         </li>
       </ul>
       <ul className="my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">
+        <li className="my-2 flex gap-2 items-center rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <UserIcon className="w-4 h-4" />
           User
         </li>
       </ul>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { Navbar } from "./Navbar/Navbar";
+export { HomeIcon } from "./Icons/Icons";
+export { GamepadIcon, WalkPathIcon, AwardIcon, UserIcon } from "./Icons/Icons";
