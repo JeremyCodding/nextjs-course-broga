f1a7dfd321ea83b10997c95cec030c9f28d84465
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-14 15:33:28 -0300

    Creating jwt for cookie session

---
+++ src/helpers/session.ts
@@ -0,0 +1,15 @@
+import { cookies } from "next/headers";
+import { decrypt } from "@/helpers/jwt";
+
+export const createSession = (payload: string) => {
+  const expires = new Date(Date.now() + 60 * 1000);
+  cookies().set("session", payload, { expires, httpOnly: true });
+};
+
+export const getSession = async (payload: string) => {
+  const session = cookies().get("session")?.value;
+
+  if (!session) return null;
+
+  return await decrypt(session);
+};
