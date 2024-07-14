0bd5f5bd5a2010f8542f59e0ffa4cb1ec1fc02a1
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-14 17:22:54 -0300

    Keeping user session

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,20 @@
-export default function UserPage() {
-  return <h1>Página do usuário</h1>;
+import { PageWrapper } from "@/components";
+import { getSession } from "@/helpers/session";
+import { redirect } from "next/navigation";
+
+export default async function UserPage() {
+  const user = await getSession();
+
+  if (!user) redirect("/auth/sign-in");
+  return (
+    <PageWrapper>
+      <div className="container mx-auto my-6">
+        <div className="w-2/3">
+          <h1 className="text-3xl my-6">{user?.name as string}</h1>
+          <h1>Página do usuário</h1>
+          <div className="flex flex-col my-6"></div>
+        </div>
+      </div>
+    </PageWrapper>
+  );
 }
