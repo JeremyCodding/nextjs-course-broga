d38c27694eb0b695c39e7922d6b8709379cad653
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-14 15:14:15 -0300

    Adding login service and control

--- src/libs/database/Users.ts
+++ src/libs/database/Users.ts
@@ -6,6 +6,9 @@ const UserDB = {
   create: async (data: any) => {
     return prisma.user.create({ data });
   },
+  findByEmail: async (email: string) => {
+    return prisma.user.findUnique({ where: { email } });
+  },
 };
 
 export default UserDB;
--- src/services/Users.ts
+++ src/services/Users.ts
@@ -1,4 +1,4 @@
-import { createHash } from "@/helpers/hash";
+import { createHash, verifyHash } from "@/helpers/hash";
 import UserDB from "@/libs/database/Users";
 
 const HOME_LATEST_COUNT = 4;
@@ -8,6 +8,17 @@ const UserService = {
     const passwordHash = await createHash(data.password);
 
     return UserDB.create({ ...data, password: passwordHash });
+  },
+  signIn: async (data: any) => {
+    const record = await UserDB.findByEmail(data.email);
+
+    if (!record) return null;
+
+    const isValidPassword = verifyHash(data.password, record.password);
+
+    if (!isValidPassword) return null;
+
+    return { ...record, password: null };
   },
 };
 
